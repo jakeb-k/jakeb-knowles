2bc7ec96dc3779c0e1a1ba62b883a8b1a6149491
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2023-10-05 13:13:24 +1000

    added icon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Jakeb Knowles</title>
         <meta name="description" content="Explore a world of dynamic web experiences through my web development portfolio. Discover a diverse range of innovative projects showcasing cutting-edge design, flawless functionality, and seamless user experiences. Let's turn your digital dreams into reality – one click at a time. All sites made by Jakeb Knowles">
+        <link rel="icon" type="image/x-icon" href="{{url('images/favicon.ico')}}">
 
         <!-- Fonts -->
         
