69f46c9c0fd28fed99109663943e20baacc9f325
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2023-10-31 13:46:13 +1000

    bug fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,12 +11,6 @@
         <!-- Fonts -->
         
         <link rel="stylesheet" href="{{asset('css/app.scss')}}" type="text/css">
-        <!--CANONICAL URL CREATOR --> 
-        @if (Str::startsWith($current = url()->current(), 'https://www.'))
-            <link rel="canonical" href="{{ str_replace('https://www.', 'https://', $current) }}">
-        @else
-            <link rel="canonical" href="{{ str_replace('https://', 'https://www.', $current) }}">
-        @endif
        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
