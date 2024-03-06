77aaff532b2bb1b13de851f2c48040670f395fc4
jakeb-k <jknow30@outlook.com>
2024-03-06 10:11:22 +1000

    view swap func

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -27,10 +27,12 @@
         </div>
 
         <div class="post-imgs"> 
-            <img class="post-mob-slide" src="{{url('images/'.$post->name.'/mobile/1.PNG')}}" style="display:block;"/>
+
+            <img class="post-mob-slide" src="{{url('images/'.$post->name.'/mobile/1.PNG')}}" />
             
-            <!-- <img class="post-web-slide" src="{{url('images/'.$post->name.'/web/1.PNG')}}" style="display:block;"/> -->
+            <img class="post-web-slide" src="{{url('images/'.$post->name.'/web/1.PNG')}}" />
             
+            <button class="swap-btn" onclick="swapView()">SWAP</button>
                 <!-- @for($i = 0; $i < $itemCount; $i++)   
                     <img class="post-img-slide" src="{{url('images/'.$post->name.'/web/'.$i.'.PNG')}}" style="{{ $i == 0 ? 'display: block;' : 'display: none;' }}" />
                 @endfor
@@ -48,34 +50,51 @@
     </div>
 </div>
 
-<!-- 
+
 <script>
-    var slideIndex = 0;
-    var slideInterval; 
-    showSlides();
+    // var slideIndex = 0;
+    // var slideInterval; 
+    // showSlides();
 
-    function setCurrentSlide(index) {
-        slideIndex = index;
-        showSlides();
+    // function setCurrentSlide(index) {
+    //     slideIndex = index;
+    //     showSlides();
+    // }
+
+    // function showSlides() {
+    //     var i;
+    //     var slides = document.getElementsByClassName("post-img-slide");
+    //     var dots = document.getElementsByClassName("nav-circle");
+
+    //     for (i = 0; i < slides.length; i++) {
+    //         slides[i].style.display = "none";
+    //         dots[i].className = dots[i].className.replace(" active", "");
+    //     }
+
+    //     slideIndex++;
+    //     if (slideIndex > slides.length) { slideIndex = 1 }
+
+    //     slides[slideIndex - 1].style.display = "block";
+    //     dots[slideIndex - 1].className += " active";
+    //     clearTimeout(slideInterval);
+    //     slideInterval = setTimeout(showSlides, 3000);
+    // }
+
+    var viewCond = true;
+    swapView(); 
+    function swapView(){
+    var mobileView = $(".post-mob-slide");
+    var webView = $(".post-web-slide");
+
+    if(viewCond) {
+        mobileView.css('display', 'none');
+        webView.css('display', 'block');
+    } else {
+        mobileView.css('display', 'block');
+        webView.css('display', 'none');
     }
-
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("post-img-slide");
-        var dots = document.getElementsByClassName("nav-circle");
-
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-
-        slideIndex++;
-        if (slideIndex > slides.length) { slideIndex = 1 }
-
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-        clearTimeout(slideInterval);
-        slideInterval = setTimeout(showSlides, 3000);
-    }
-</script> -->
+    
+    viewCond = !viewCond;
+}
+</script>
 @endsection
